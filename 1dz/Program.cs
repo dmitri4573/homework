08873f1c0149e218310bb17d03fb0f3dd439b5da
dmitri <demoon12@mail.ru>
2024-05-25 00:38:42 +0300

    рекурсия

---
+++ 1dz/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+
+Console.Write("Введите число M: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        
+
+    }
+    else
+    {
+        Console.Write(m + " ");
+        PrintNaturalNumbers(m - 1, n);
+    }
+}
+
+PrintNaturalNumbers(m, n);
