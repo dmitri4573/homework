ab05db0a72d91e52c0bc53776689c5609fec76ec
dmitri <demoon12@mail.ru>
2024-05-25 11:41:38 +0300

    рещение 3

---
+++ 3dz/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//  Использовать рекурсию, не использовать циклы.
+
+int[] Array = { 5, 35, 27, 38, 87 }; 
+
+void PrintArrayRev(int[] arr, int index)
+{
+    if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayRev(arr, index - 1); 
+        } 
+}
+
+Console.WriteLine("Элементы массива, начиная с конца:");
+PrintArrayRev(Array, Array.Length - 1);
